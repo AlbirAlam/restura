effe4658cb01778dda60d5e9d85d902f9014a0c7
Albir_Alam <albiralam864@gmail.com>
2024-05-07 14:23:59 +0600

    Restura project

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//-------- auto-counter start --------//
+
+jQuery(document).ready(function ($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+});
+
+//-------- auto-counter end --------//
+
+
+
+//--------filter start--------//
+var mixer = mixitup('.filter-js');
+//--------filter end--------//
+
+
